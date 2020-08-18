96f456c7bdf6585f05180bb10399fb3926dde86f
Robert Dam <robert.dam@office.ziggo.nl>
2020-08-18 22:15:58 +0200

    Added Person Controller and Model to save the data from the API.

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,9 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Person;
+use App\Classes\Swapi;
 
 class PersonController extends Controller
 {
-    //
+    public function import()
+    {
+        $api = new Swapi;
+
+        if (empty($persons = $api->request('people')))
+        {
+            return redirect('/')->with('failure', 'Could not fetch data from the API, please try again.');
+        }
+
+        foreach ($persons as $person)
+        {
+            $id = $api->fetchIdFromUrl($person->url);
+
+            $people             = Person::query()->firstOrCreate(['id' => $id]);
+            $people->planet_id  = $api->fetchIdFromUrl($person->homeworld);
+            $people->name       = $person->name;
+            $people->height     = $person->height;
+            $people->mass       = $person->mass;
+            $people->hair_color = $person->hair_color;
+            $people->skin_color = $person->skin_color;
+            $people->eye_color  = $person->eye_color;
+            $people->birth_year = $person->birth_year;
+            $people->gender     = $person->gender;
+            $people->created    = $person->created;
+            $people->edited     = $person->edited;
+
+            $people->save();
+        }
+
+        dd($persons);
+    }
 }
--- app/Person.php
+++ app/Person.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Person extends Model
 {
-    //
+    protected $table = 'persons';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'planet_id', 'name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender', 'created', 'edited', 'created_at',
+        'updated_at',
+    ];
+
 }
