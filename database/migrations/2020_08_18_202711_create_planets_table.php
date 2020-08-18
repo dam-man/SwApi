40a756a77733100a5670e62f4b11117296592851
Robert Dam <robert.dam@office.ziggo.nl>
2020-08-18 22:35:58 +0200

    Updated planets migration file

--- database/migrations/2020_08_18_202711_create_planets_table.php
+++ database/migrations/2020_08_18_202711_create_planets_table.php
@@ -15,14 +15,16 @@ class CreatePlanetsTable extends Migration
     {
         Schema::create('planets', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
-            $table->string('climate')->nullable();
-            $table->string('terrain')->nullable();
-            $table->integer('diameter ')->default(0);
+            $table->string('name', 255)->nullable();
+            $table->string('climate', 255)->nullable();
+            $table->string('terrain', 255)->nullable();
+            $table->integer('diameter')->default(0);
             $table->integer('population')->default(0);
             $table->mediumInteger('rotation_period')->default(0);
             $table->mediumInteger('orbital_period')->default(0);
-            $table->smallInteger('surface_water ')->default(0);
+            $table->smallInteger('surface_water')->default(0);
+            $table->string('created', 255);
+            $table->string('edited', 255);
             $table->timestamps();
         });
     }
