30a66f991be54152fc6af61b272ce8d4977cf3f7
Robert Dam <robert.dam@office.ziggo.nl>
2020-08-20 09:15:27 +0200

    Updated Specie model to insert relations with people

--- app/Http/Controllers/SpecieController.php
+++ app/Http/Controllers/SpecieController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Classes\Swapi;
 use App\Specie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SpecieController extends Controller
 {
@@ -16,6 +17,10 @@ class SpecieController extends Controller
         {
             return redirect('/')->with('failure', 'Could not fetch data from the API, please try again.');
         }
+
+        // Remove current items.
+        // Todo Making a sync functionality which is also available
+        DB::table('species_to_people')->truncate();
 
         foreach ($species as $specie)
         {
@@ -35,8 +40,32 @@ class SpecieController extends Controller
             $people->edited           = $specie->edited;
 
             $people->save();
+
+            if ( ! empty($specie->people))
+            {
+                if(!$this->updatePeopleToSpecieTable($id, $specie->people))
+                {
+                    return redirect('/')->with('failure', 'Could not import species to people from the API.');
+                }
+            }
         }
 
         return redirect('/')->with('success', 'People has been imported from the starwars API.');
+    }
+
+    private function updatePeopleToSpecieTable($specie_id, $people)
+    {
+        $api     = new Swapi;
+        $inserts = [];
+
+        foreach ($people as $people_id)
+        {
+
+            $people_id = $api->fetchIdFromUrl($people_id);
+
+            $inserts[] = ['specie_id' => $specie_id, 'people_id' => $people_id];
+        }
+
+        return DB::table('species_to_people')->insert($inserts);
     }
 }
